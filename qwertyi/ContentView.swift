5469fbd924360b62c828e74c0fb4e672b8c0c0a2
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-05-24 15:54:18 +0530

    Initial Commit

---
+++ qwertyi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qwertyi
+//
+//  Created by Test Ios on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qwertyi/qwertyiApp.swift
@@ -0,0 +1,17 @@
+//
+//  qwertyiApp.swift
+//  qwertyi
+//
+//  Created by Test Ios on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct qwertyiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
